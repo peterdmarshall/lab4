155b3f4816752afbd1d8f85229eab7d4738282df
Peter <pdmarsha@edu.uwaterloo.ca>
2019-03-30 12:22:27 -0400

    Initial commit

---
+++ lab4_priority_queue.cpp
@@ -0,0 +1,61 @@
+#include "lab4_priority_queue.hpp"
+
+using namespace std;
+
+// PURPOSE: Parametric constructor 
+// initializes heap to an array of (n_capacity + 1) elements
+PriorityQueue::PriorityQueue(unsigned int n_capacity) {
+	heap = new TaskItem*[n_capacity + 1];
+}
+
+// PURPOSE: Explicit destructor of the class PriorityQueue
+PriorityQueue::~PriorityQueue() {
+	for(int i = 0; i < size; i++) {
+		if(heap[i] != NULL) {
+			heap[i] = NULL;
+			delete heap[i];
+		}
+	}
+	delete [] heap;
+}
+
+// PURPOSE: Returns the number of elements in the priority queue
+unsigned int PriorityQueue::get_size() const {
+	return size;
+}
+
+// PURPOSE: Returns true if the priority queue is empty; false, otherwise
+bool PriorityQueue::empty() const {
+	return size == 0;
+}
+
+// PURPOSE: Returns true if the priority queue is full; false, otherwise
+bool PriorityQueue::full() const {
+	return false;
+}
+
+// PURPOSE: Prints the contents of the priority queue; format not specified
+void PriorityQueue::print() const {
+}
+
+// PURPOSE: Returns the max element of the priority queue without removing it
+// if the priority queue is empty, it returns (-1, "N/A")
+PriorityQueue::TaskItem PriorityQueue::max() const {
+	return TaskItem(-1, "NULL");
+}
+
+// PURPOSE: Inserts the given value into the priority queue
+// re-arranges the elements back into a heap
+// returns true if successful and false otherwise
+// priority queue does not change in capacity
+bool PriorityQueue::enqueue( TaskItem val ) {
+	return false;
+}
+
+// PURPOSE: Removes the top element with the maximum priority
+// re-arranges the remaining elements back into a heap
+// returns true if successful and false otherwise
+// priority queue does not change in capacity
+bool PriorityQueue::dequeue() {
+	return false;
+}
